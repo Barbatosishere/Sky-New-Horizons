739bfdf125cb8e4a3b039ec635580f2afdb68702
Barbatosishere <584146143@qq.com>
2026-07-11 14:46:11 +0800

    更新了mods，并删除了几个无用mods以及bug修复
    Updated the mods, removed a few useless ones, and fixed some bugs

---
+++ kubejs/server_scripts/Ex_Deorum/ltem_ex_acquisition_flowers.js
@@ -0,0 +1,91 @@
+ServerEvents.recipes(event => {
+        const TagKey = Java.loadClass('net.minecraft.tags.TagKey');
+        const BuiltInRegistries = Java.loadClass('net.minecraft.core.registries.BuiltInRegistries');
+        const ResourceLocation = Java.loadClass('net.minecraft.resources.ResourceLocation');
+        const blockRegistry = BuiltInRegistries.BLOCK;
+        const itemRegistry = BuiltInRegistries.ITEM;
+        const tagKey = TagKey.create(blockRegistry.key(), new ResourceLocation('minecraft', 'flowers'));
+        const flowers = [];
+        blockRegistry.getTag(tagKey).orElseThrow()
+            .forEach(holder => {
+                const id = holder.key().location();
+                if (itemRegistry.containsKey(id)) {
+                    flowers.push(id.toString());
+                }
+            });
+        const moss = 'minecraft:moss_block';
+
+        flowers.forEach(chunkId => {
+    event.custom({
+                type: 'exdeorum:sieve',
+                ingredient: { item: moss },
+                mesh: { item: 'exdeorum:string_mesh' },
+                result: { id: chunkId },
+                result_amount: {
+            type: 'minecraft:binomial',
+            n: 1.0,
+            p: 0.01
+        }
+            });
+
+    event.custom({
+            type: 'exdeorum:sieve',
+            ingredient: { item: moss },
+            mesh: { item: 'exdeorum:flint_mesh' },
+            result: { id: chunkId },
+            result_amount: {
+                type: 'minecraft:binomial',
+                n: 1.0,
+                p: 0.05
+            }
+        });
+
+    event.custom({
+        type: 'exdeorum:sieve',
+        ingredient: { item: moss },
+        mesh: { item: 'exdeorum:iron_mesh' },
+        result: { id: chunkId },
+        result_amount: {
+            type: 'minecraft:binomial',
+            n: 1.0,
+            p: 0.10
+        }
+    });
+
+    event.custom({
+        type: 'exdeorum:sieve',
+        ingredient: { item: moss },
+        mesh: { item: 'exdeorum:golden_mesh' },
+        result: { id: chunkId },
+        result_amount: {
+            type: 'minecraft:binomial',
+            n: 1.0,
+            p: 0.15
+        }
+    });
+
+    event.custom({
+        type: 'exdeorum:sieve',
+        ingredient: { item: moss },
+        mesh: { item: 'exdeorum:diamond_mesh' },
+        result: { id: chunkId },
+        result_amount: {
+            type: 'minecraft:binomial',
+            n: 1.0,
+            p: 0.20
+        }   
+      });
+
+    event.custom({
+        type: 'exdeorum:sieve',
+        ingredient: { item: moss },
+        mesh: { item: 'exdeorum:netherite_mesh' },
+        result: { id: chunkId },
+        result_amount: {
+            type: 'minecraft:binomial',
+            n: 1.0,
+            p: 0.3
+        }
+    });
+    })
+    });
